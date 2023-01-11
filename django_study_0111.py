f419ac973e1ec7e04817c0ae4854c7a3dffe3019
suahoo7 <hansua9977@gmail.com>
2023-01-11 16:29:52 +0900

    change

---
+++ django_study_0111.py
@@ -0,0 +1,9 @@
+#django process
+
+# 1) django-admin startproject django_project1 .
+# 2) python manage.py runserver
+# 3) 서버 접속
+# 4) 관리자 계정 생성: python manage.py migrate
+# 5) python manage.py createsuperuser
+# 6) python manage.py runserver
+# 7) 관리자 페이지 로그인 /admin/
